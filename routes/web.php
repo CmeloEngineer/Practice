71f54fea8f2f5a257d2c6b37046946d45d2c92a3
Cmelo <cmelo.engineer@gmail.com>
2023-01-29 16:39:18 +0900

    breezeのインストール

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PracticeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//一覧画面を表示
-Route::get('/practice', [PracticeController::class, 'showList'])->name('practice');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
